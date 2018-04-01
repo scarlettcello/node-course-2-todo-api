adb7160581a7878861486c42d9d96fd4c854752e
Hyoeun Lee <scarlettcello@gmail.com>
2018-04-02 01:19:59 +0900

    Setup app for heroku

--- server/server.js
+++ server/server.js
@@ -7,8 +7,7 @@ var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
 
 var app = express();
-const port = 'mongodb://scarlettcello:gys0925@ds131329.mlab.com:31329/learning'
-              || 3000;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
