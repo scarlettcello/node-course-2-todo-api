1c1ab18de9c2c39afab44f3e059739df0a4c127f
Hyoeun Lee <scarlettcello@gmail.com>
2018-04-02 01:04:47 +0900

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,6 +1,9 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost:27017/TodoApp');
+mongoose.connect(
+  'mongodb://<dbuser>:<dbpassword>@ds131329.mlab.com:31329/learning' ||
+  'mongodb://localhost:27017/TodoApp'
+);
 
 module.exports = {mongoose};
