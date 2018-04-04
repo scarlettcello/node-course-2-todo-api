e36fe165812acbddb1691f2995480f80e01fc833
Hyoeun Lee <scarlettcello@gmail.com>
2018-04-04 21:49:58 +0900

    Setup app for heroku

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,8 +1,11 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
+
+//'mongodb://admin:gys0925@ds131329.mlab.com:31329/learning' ||
+
 mongoose.connect(
-  'mongodb://scarlettcello:gys0925@ds131329.mlab.com:31329/learning' ||
+  process.env.MONGOLAB_URI ||
   'mongodb://localhost:27017/TodoApp'
 );
 
